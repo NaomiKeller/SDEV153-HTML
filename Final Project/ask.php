36d9e08c555c209bed7b4421aa7985f25a16e939
Naomi Keller <thatstraveling@gmail.com>
2020-06-24 21:55:33 -0400

    inital commit

---
+++ Final Project/ask.php
@@ -0,0 +1,20 @@
+<html>
+ <!-- listen i tried for 4 hours to try and figure out PHP and i give up so this form doesn't actually do anything-->
+<head>
+<script>
+function goBack() {
+  window.history.back();
+}
+</script> 
+</head>
+<link rel="stylesheet" type="text/css" href="css.css">
+
+<div class="column2" style="width:800px; margin:0 auto;">
+<div class="box" style="margin-right:7em">
+<h2>Thank you! Your question was received.</h2>
+<button onclick="goBack()">Go Back</button>
+
+</div>
+</div>
+
+</html>
